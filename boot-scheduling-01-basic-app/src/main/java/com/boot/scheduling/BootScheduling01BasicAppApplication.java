bfdfb7300a86308142bbfeb4e496ee477100a2db
codeonce-with-akash <akashodwa4@gmail.com>
2024-08-11 12:22:58 +0530

    first commit

---
+++ boot-scheduling-01-basic-app/src/main/java/com/boot/scheduling/BootScheduling01BasicAppApplication.java
@@ -0,0 +1,13 @@
+package com.boot.scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootScheduling01BasicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootScheduling01BasicAppApplication.class, args);
+	}
+
+}
---
+++ boot-scheduling-01-basic-app/src/test/java/com/boot/scheduling/BootScheduling01BasicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootScheduling01BasicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
